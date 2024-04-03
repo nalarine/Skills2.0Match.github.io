cb9475802c973a32dfe3645c9170ab70cfbc8723
Jerico Rillo <jericorillo@192.168.1.3>
2024-04-04 00:56:43 +0800

    bring back company in jobs schema

--- user/server/models/jobsModel.js
+++ user/server/models/jobsModel.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const jobSchema = new mongoose.Schema(
   {
     name: { type: Schema.Types.ObjectId, ref: "Companies" },
+    company: { type: Schema.Types.ObjectId, ref: "Companies" },
     jobTitle: { type: String, required: [true, "Job Title is required"] },
     jobType: { type: String, required: [true, "Job Type is required"] },
     location: { type: String, required: [true, "Location is required"] },
