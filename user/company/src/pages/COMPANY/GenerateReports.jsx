b68c3ab2dd33e1a1f9ef2d9c7ac2677aff814fc7
nalarine <qaksison@tip.edu.ph>
2024-05-04 03:59:05 +0800

    add

--- user/company/src/pages/COMPANY/GenerateReports.jsx
+++ user/company/src/pages/COMPANY/GenerateReports.jsx
@@ -211,7 +211,7 @@ export default function GenerateReports() {
             </span>
             <input
               type="text"
-              placeholder="Search"
+              placeholder="Searches"
               className="block w-full py-1.5 pl-12 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               onChange={(e) => setSearchTerm(e.target.value)}
             />
