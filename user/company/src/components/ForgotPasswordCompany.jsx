44e3339420bcf7cf83972f13147399a95d79f989
nalarine <qaksison@tip.edu.ph>
2024-06-04 12:32:26 +0800

    FIXED FORGOT PASSWORD

---
+++ user/company/src/components/ForgotPasswordCompany.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import Logo from '../assets/logo.png';
+import ForgotPic from '../assets/forgot-password.png';
+import { apiRequest } from '../utils';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Checkbox } from "@nextui-org/react";
+import { Input } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+
+const ChangePasswordForm = () => {
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true); 
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [newPasswordVisible, setNewPasswordVisible] = useState(false); // State for new password visibility
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false); // State for confirm password visibility
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setErrorMessage("Passwords don't match");
+      return;
+    }
+
+    if (!acceptedTerms) {
+        setErrorMessage('Please accept the terms of service');
+        return;
+      }
+  
+
+    try {
+      const response = await apiRequest({
+        url: '/companies/reset-password-token/:token',
+        method: 'POST',
+        data: { email, newPassword }
+      });
+      if (response.success) {
+        setSuccessMessage(response.message);
+        setShowModal(true);
+      } else {
+        setErrorMessage(response.message);
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('Internal Server Error');
+    }
+  };
+  const closeModal = () => {
+    setShowModal(false);
+    // Additional cleanup or redirection logic after closing modal if needed
+  };
+  return (
+    <section className="bg-gradient-to-r from-green-500 to-orange-900 h-screen overflow-hidden">
+      <div className="flex flex-col md:flex-row items-center justify-start px-4 md:px-12 py-8 mx-auto md:h-full lg:py-0">
+        <div className="w-full md:w-auto p-10 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          <a href="#" className="flex items-center mb-4 text-xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-10 h-10 mr-2" src={Logo} alt="logo"/>
+            Skills 2.0 Match    
+          </a>
+          <h2 className="mb-1 text-xl font-bold text-left leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Create new password
+          </h2>
+          <p className="mb-4 text-sm text-gray-600 dark:text-gray-400 text-left">Your new password must be different from previously used passwords.</p>
+          <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5 text-left" onSubmit={handleSubmit}>
+            {/* Display error message if there is one */}
+            {errorMessage && <div className="text-red-600">{errorMessage}</div>}
+            {/* Display success message if password is reset successfully */}
+            {successMessage && <div className="text-green-600">{successMessage}</div>}
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+             <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className={`bg-gray-50 border ${isValidEmail ? 'border-gray-300' : 'border-red-500'} text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  placeholder="name@company.com"
+                  required
+                  value={email}
+                  onChange={(e) => {
+                    const enteredEmail = e.target.value;
+                    setEmail(enteredEmail);
+                    const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+                    setIsValidEmail(emailRegex.test(enteredEmail));
+                  }}
+                />
+                {!isValidEmail && <p className="text-red-500 text-sm mt-1">Please enter a valid email ending with "@gmail.com"</p>}
+            </div>
+            <div>
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+          <Input.Password
+            id="password"
+            name="password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            iconRender={visible => (visible ? <EyeTwoTone onClick={() => setNewPasswordVisible(!newPasswordVisible)} style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }} /> : <EyeInvisibleOutlined onClick={() => setNewPasswordVisible(!newPasswordVisible)} style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }} />)} // Toggle password visibility
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+          <Input.Password
+            id="confirm-password"
+            name="confirm-password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            iconRender={visible => (visible ? <EyeTwoTone onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)} style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }} /> : <EyeInvisibleOutlined onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)} style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }} />)} // Toggle password visibility
+          />
+        </div>
+            <div className="flex items-start">
+              <Checkbox
+                id="accept-terms"
+                color="success"
+                checked={acceptedTerms}
+                onChange={(e) => setAcceptedTerms(e.target.checked)}
+              />
+               <span className="text-sm text-gray-600 dark:text-gray-400 text-left">
+                  I accept to Skills 2.0 Match's {' '}
+                  <a
+                    href="/privacy-policy"
+                    className="font-medium text-green-600 hover:underline dark:text-green-500"
+                  >
+                    Privacy Policy
+                  </a>{' '}
+                  and{' '}
+                  <a
+                    href="/terms-of-service"
+                    className="font-medium text-green-600 hover:underline dark:text-green-500"
+                  >
+                    Terms of Service
+                  </a>
+                </span>
+            </div>
+            <div className="mt-2">
+            <button type="submit" className="w-48 text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reset password</button>
+              <a href="/user-auth" className="text-green-600 hover:underline ml-4">Back to login</a>
+            </div>
+          </form>
+        </div>
+        <div className="hidden md:flex md:w-3/5 justify-end items-end">
+          <div className="flex justify-end w-full">
+            <img src={ForgotPic} alt="template" className="w-full h-full object-cover" style={{ marginRight: '50px', marginTop: '50px', width: '80%' }} />
+          </div>
+        </div>
+      </div>
+      <Modal backdrop={showModal ? 'blur' : 'opaque'} isOpen={showModal} onClose={closeModal}>
+        <ModalContent>
+          <ModalHeader>Password Reset Successful</ModalHeader>
+          <ModalBody>
+            <p>Your password has been successfully reset.</p>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="success" onClick={closeModal}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </section>
+  );
+};
+
+export default ChangePasswordForm;
---
+++ user/server/sendVerificationEmailCompany.js
@@ -0,0 +1,119 @@
+// Import necessary modules
+import nodemailer from 'nodemailer';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path';
+
+// Get the directory name of the current module file
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Create a transporter with Gmail SMTP configuration
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'skills2match2024@gmail.com',
+    pass: 'hcxy bjfv tmcv vcuo',
+  },
+});
+
+// Function to send forgot password email
+export const sendForgotPasswordEmail = async (user) => {
+  const subject = "Reset Your Password";
+  const resetPasswordLink = `http://localhost:5173/reset-password-token/${user.resetPasswordToken}`;
+  const headerImagePath = path.join(__dirname, '..', 'company', 'src', 'assets', 'header-password.png');
+  const logoPath = path.join(__dirname, '..', 'company', 'src', 'assets', 'logo.png');
+  const message = `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: 'Times New Roman', serif;
+            color: #333;
+            margin: 0;
+            padding: 0;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            text-align: left;
+          }
+          .header img {
+            width: 100%;
+            height: auto;
+          }
+          .content {
+            background-color: #f7f7f7;
+            padding: 20px;
+            border-radius: 8px;
+            margin-top: 20px;
+          }
+          .button {
+            display: inline-block;
+            background-color: transparent;
+            color: #000000; /* Green text color for 'Verify Account' */
+            text-decoration: none;
+            padding: 10px 18px;
+            border-radius: 30px;
+            font-size: 16px;
+            margin-top: 20px;
+            transition: background-color 0.3s;
+            border: 2px solid black; /* Black outline */
+          }
+          .button:hover {
+            background: linear-gradient(90deg, rgba(214,176,180,1) 15%, rgba(179,176,248,1) 52%, rgba(240,224,236,1) 100%);
+            color: #fff; /* Change text color to white on hover */
+          }
+          .footer {
+            margin-top: 20px;
+            text-align: left;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <img src="cid:headerImage" alt="Header Image">
+          </div>
+          <div class="content">
+            <p>Hello! <strong>${user.name}</strong>,</p>
+            <p>We've received a request to reset your password.</p>
+            <p>Please click the button below to reset your password. The link will expire after one hour.</p>
+            <a href="${resetPasswordLink}" class="button">Reset Password</a>
+            <p>If you didn't request for a password reset, don't worry, we haven't done anything yet; feel free to disregard this email.</p>
+            <p>Thanks for using Skills2.0Match.</p>
+          </div>
+          <div class="footer">
+            <p>Best,</p>
+            <p>Skills2.0Match</p>
+            <img src="cid:logo" alt="Skills 2.0 Match Logo" style="width: 100px; height: auto;">
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
+
+  try {
+    await transporter.sendMail({
+      from: 'Skills 2.0 Match <skills2match2024@gmail.com>',
+      to: user.email,
+      subject: subject,
+      html: message,
+      attachments: [
+        {
+          filename: 'header-password.jpg',
+          path: headerImagePath,
+          cid: 'headerImage'
+        },
+        {
+          filename: 'logo.png',
+          path: logoPath,
+          cid: 'logo'
+        }
+      ]
+    });
+
+    console.log(`Forgot password email sent to ${user.email}`);
+  } catch (error) {
+    console.error("Error sending forgot password email:", error);
+  }
+};
